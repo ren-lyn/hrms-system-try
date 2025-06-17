1e4163ffed9148800937dd85eacf239d31534b61
ren-lyn <concinarenelyn86@gmail.com>
2025-06-17 18:26:46 +0800

    Remove HrAssistantDashboard component as part of project restructuring

--- hrms-frontend/src/pages/HrAssistantDashboard.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const HrAssistantDashboard = () => {
-  return <h2>Welcome to the HR Assistant Dashboard</h2>;
-};
-
-export default HrAssistantDashboard;
-// This code defines a simple React component for the HR Assistant Dashboard.
