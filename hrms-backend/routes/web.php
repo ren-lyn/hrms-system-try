98b02102f389331d6f0265068deef1ae4ab51857
ren-lyn <concinarenelyn86@gmail.com>
2025-08-22 16:13:55 +0800

    feat: Add sessions migration to create sessions table for user session management

--- hrms-backend/routes/web.php
+++ hrms-backend/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/check', function () {
     return response()->json(['status' => 'Web route working']);
 });
