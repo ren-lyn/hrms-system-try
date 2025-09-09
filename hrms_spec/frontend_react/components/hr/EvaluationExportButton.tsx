bca97cd1d63b65710d91e8a43d3f6d4a7f24bd31
allenjasper <allenararao33@gmail.com>
2025-09-09 17:31:15 +0800

    Delete hrms_spec directory

--- hrms_spec/frontend_react/components/hr/EvaluationExportButton.tsx
+++
@@ -1,8 +0,0 @@
-export function EvaluationExportButton({ evaluationId }:{ evaluationId: number }) {
-	function openExport() {
-		window.open(`/api/performance/reviews/${evaluationId}/export`, '_blank');
-	}
-	return (
-		<button onClick={openExport} className="px-3 py-1 bg-gray-700 text-white rounded">Export</button>
-	);
-}
